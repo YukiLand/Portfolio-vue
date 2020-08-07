0619276852d0368248fc8c49cf98a30c0f2ae9c8
Antoine Lecoffre <antoine.lecoffre@orange.fr>
2020-08-07 11:23:33 +0200

    navbar + button github non fonctionnel

---
+++ src/components/navbar.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-app-bar
+      color="white"
+      dense
+      white
+    >
+
+      <v-toolbar-title>Menu</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <span>Qui suis-je</span>
+
+      <v-spacer></v-spacer>
+
+      <span>Professionnel</span>
+
+      <v-spacer></v-spacer>
+
+      <span>Contact</span>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon class="ma-2" outlined fab color="red">
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+
+      <button @click="github">click</button>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+            class="ma-2" outlined fab color="indigo"
+          >
+            <v-icon>mdi-flag</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar> 
+</template>
+
+<script>
+  export default {
+    name: 'navbar',
+
+    data: () => ({
+
+    }),
+    methods: () => ({
+      github: function() {
+        window.open("https://github.com", "_blank"); 
+      }
+    })
+  }
+</script>
